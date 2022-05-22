456e5be03148a6e88301926274a637ab4bc5193b
Pavel Marmurok <pavel.marmurok@gmail.com>
2022-05-23 01:32:40 +0300

    feat: solve task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+
+const secretFolder = './03-files-in-folder/secret-folder';
+
+fs.readdir(secretFolder, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  files.forEach((file) => {
+    const filePath = path.normalize(`${secretFolder}/${file}`);
+    fs.stat(filePath, (statError, statFile) => {
+      if (statError) {
+        throw statError;
+      }
+      if (!statFile.isDirectory()) {
+        const extention = path.extname(file);
+        console.log(`${path.basename(file, extention)} - ${extention.slice(1)} - ${statFile.size / 1000}kb`);
+      }
+    });
+  });
+});
